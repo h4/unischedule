12b9a66cbf6dd62fa51eb1926709348f7da1a189
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-09-09 03:31:26 +0400

    Чистим subtitle на странице преподавателей

--- src/app/js/teachers/teachersController.js
+++ src/app/js/teachers/teachersController.js
@@ -1,6 +1,7 @@
 var unisheduleApp = angular.module('unisheduleApp');
 
 unisheduleApp.controller('teachersController', function($scope, $rootScope) {
+    $rootScope.subtitle = "";
     $rootScope.tabLocation = '/teachers';
     $scope.teachers = [];
     $scope.title = "Список преподавателей";
