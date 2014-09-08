20f32d7022b1b4af760a04c749bf86d62ea055fd
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-09-08 23:55:50 +0400

    Добавил счётчик инициализированных запросов к серверу

--- src/app/js/loading/loadingDirective.js
+++ src/app/js/loading/loadingDirective.js
@@ -2,19 +2,26 @@ var unisheduleApp = angular.module('unisheduleApp');
 
 unisheduleApp
     .directive('loading', function () {
+        var that = this;
+        that.processCount = 0;
 
         return {
             restrict: 'E',
-            link : function(scope, element, attrs) {
-                    scope.$on("loading-started", function(e) {
-                        element.addClass('loading_process');
-                    });
+            link: function (scope, element, attrs) {
 
-                    scope.$on("loading-complete", function(e) {
+                scope.$on("loading-started", function (e) {
+                    that.processCount++;
+                    element.addClass('loading_process');
+                });
+
+                scope.$on("loading-complete", function (e) {
+                    that.processCount--;
+                    if (that.processCount === 0) {
                         element.removeClass('loading_process');
-                    });
+                    }
+                });
 
-                }
+            }
         };
     });
 
