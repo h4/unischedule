a9abd3a8da1f48fa3d55b49203c296e2f137dc66
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-09-11 11:21:20 +0400

    Fixed setting kiosk cookie

--- src/app/js/faculty/facultyController.js
+++ src/app/js/faculty/facultyController.js
@@ -3,13 +3,16 @@ var unisheduleApp = angular.module('unisheduleApp');
 unisheduleApp.controller('FacultyCtrl',
     ['$scope', '$rootScope', '$http', '$location', 'APIUrls',
         function ($scope, $rootScope, $http, $location, APIUrls) {
+            var kiosk = $location.search().kiosk;
             $scope.faculties = [];
             $rootScope.subtitle = "";
             $rootScope.tabLocation = '/';
+
             $location.search({
                 'date': null,
                 'q': null,
-                'kind': null
+                'kind': null,
+                'kiosk': kiosk
             });
 
             $http
